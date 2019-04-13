13e84e0ee95e736accd5be96f863606bcc3b2de5
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2019-04-13 10:14:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/button_counter_baczmaga/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.button_counter_baczmaga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
